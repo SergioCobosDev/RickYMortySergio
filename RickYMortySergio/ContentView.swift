86f3411eecd9b79ede5175be2d86dd32e91b7757
Sergio Cobos <01eidietico-seres@icloud.com>
2024-04-25 17:29:11 +0200

    Initial Commit

---
+++ RickYMortySergio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickYMortySergio
+//
+//  Created by Sergio Cobos on 25/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickYMortySergio/RickYMortySergioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickYMortySergioApp.swift
+//  RickYMortySergio
+//
+//  Created by Sergio Cobos on 25/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RickYMortySergioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
